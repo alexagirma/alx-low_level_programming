c6e323598c19d87bde6627f5f7e4888b7be2f9b5
Alexagirma <alexagirma255@gmail.com>
2021-11-29 05:47:24 -0800

    dynamic libraries

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,4 +1,4 @@
-#include "main.h"
+
 
 /**
 * _isupper - Check if a letter is upper
@@ -8,7 +8,7 @@
 */
 int _isupper(int x)
 {
-if (x >+ 65 && x <= 90)
+if (x >= 65 && x <= 90)
 {
 return (1);
 }
--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -8,7 +8,7 @@
 */
 int _isdigit(int x)
 {
-  if (x >= 48 && x <= 57)
+if (x >= 48 && x <= 57)
 {
 return (1);
 }
