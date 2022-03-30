67994cd07cfb0d62f09b38ce78efb90929b0727d
Alexagirma <alexagirma255@gmail.com>
2022-03-30 13:17:58 -0700

    alogrithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,43 +1,38 @@
 #include "search_algos.h"
 
 /**
-  * interpolation_search - Searches for a value in a sorted array
-  *                        of integers using interpolation search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
-  *
-  * Description: Prints a value every time it is compared in the array..
-  */
+ * interpolation_search - searches for a value in a sorted array of integers
+ * using the Interpolation search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: the first index where value is located, or -1 on failure
+ */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t i, l, r;
+	size_t l, m, r;
 
-	if (array == NULL)
-		return (-1);
-
-	for (l = 0, r = size - 1; r >= l;)
+	if (array != NULL && size > 0)
 	{
-		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
-		if (i < size)
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		else
+		l = 0;
+		r = size - 1;
+		while (array[l] != array[r])
 		{
-			printf("Value checked array[%ld] is out of range\n", i);
-			break;
+			m = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
+			if (m < l || m > r)
+			{
+				printf("Value checked array[%lu] is out of range\n", m);
+				break;
+			}
+			printf("Value checked array[%lu] = [%d]\n", m, array[m]);
+			if (array[m] < value)
+				l = m + 1;
+			else if (array[m] > value)
+				r = m - 1;
+			else
+				return (m);
 		}
-
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			r = i - 1;
-		else
-			l = i + 1;
 	}
-
-	
-return (-1);
+	return (-1);
 }
